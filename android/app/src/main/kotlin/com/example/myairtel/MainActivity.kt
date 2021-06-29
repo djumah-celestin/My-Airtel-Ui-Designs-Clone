c2ab8a9cd28f659938df85fd543e3429d4fc937b
djumah-celestin <celestindjumah3@gmail.com>
2021-06-29 18:59:29 +0200

    Project Initialization

---
+++ android/app/src/main/kotlin/com/example/myairtel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myairtel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyAirtel());
+
+class MyAirtel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Airtel',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DeviceSimulator Demo'),
+        ),
+        body: Center(
+          child: Text('Hello multiple resolutions!'),
+        ),
+      ),
+    );
+  }
+}

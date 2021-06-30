150fd6ae0f98032ac9ef327246ae01914b2aa13b
djumah-celestin <celestindjumah3@gmail.com>
2021-06-30 19:25:34 +0200

    Accueil Screen Header Pictures implementation

--- lib/Screens/Accueil.dart
+++ lib/Screens/Accueil.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Accueil extends StatefulWidget {
   @override
@@ -9,7 +11,57 @@ class _AccueilState extends State<Accueil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Bb"),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height / 4,
+                viewportFraction: 1.0,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.ease,
+                enlargeCenterPage: true,
+                // onPageChanged: callbackFunction,
+                scrollDirection: Axis.horizontal,
+              ),
+              items: [
+                'assets/images/maxresdefault.jpg',
+                'assets/images/My-Airtel-App.jpg',
+              ].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                          top: ScreenUtil().setHeight(10.0),
+                          left: 0.0,
+                          right: 0.0),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(i),
+                              fit: BoxFit.fill,
+                            ),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.zero,
+                                topRight: Radius.zero,
+                                bottomLeft: Radius.zero,
+                                bottomRight: Radius.zero)),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
